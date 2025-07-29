781de26c6acf56b3ff9ae79da72932eb896a04cf
Heth Chheda <heth.chheda@ritetechnologies.co.in>
2025-07-29 23:17:59 +0530

    fixes optimizations

--- lib/features/User/viewmodel/home_view_model.dart
+++ lib/features/User/viewmodel/home_view_model.dart
@@ -19,7 +19,6 @@ class HomeViewModel extends ChangeNotifier {
       '${_homeData?.currentUser?.firstName ?? ''} ${_homeData?.currentUser?.lastName ?? ''}'
           .trim();
   String get userEmail => _homeData?.currentUser?.email ?? '';
-  String get userMobile => _homeData?.currentUser?.mobile ?? '';
   int get leaveBalance => _homeData?.currentUser?.balance ?? 0;
   int get approvedLeaves => _homeData?.currentUser?.approved ?? 0;
   int get rejectedLeaves => _homeData?.currentUser?.rejected ?? 0;
